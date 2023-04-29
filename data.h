dd60dab22a8eed7b41572e32e2d8e40fd09f91e5
Andrey Perevitskiy <andrew.perevitsky@gmail.com>
2023-04-29 10:22:41 +0300

    Intuitive focus in login form

--- data.h
+++ data.h
@@ -1,47 +1,47 @@
 #ifndef DATA_H
-#define DATA_H
 
 #include <gtk/gtk.h>
 #include <mysql.h>
 
-/* A data which will passed everywhere the auxiliary objects is needed. */
+// A data which will passed everywhere the auxiliary objects is needed
 struct wrap_data {
-    GObject *app;
-    GObject *win;
+    GObject * app;
+    GObject * win;
     gpointer data;
 };
 
 struct con_data {
-    GtkWidget *host;
-    GtkWidget *uname;
-    GtkWidget *passw;
-    GList *clist; /* Keep an array of the server structures. */
-    MYSQL *con;
+    GtkWidget * host;
+    GtkWidget * uname;
+    GtkWidget * passw;
+    GList * clist; // Keep an array of the server structures
+    MYSQL * con;
 
-    /* An extra. */
-    GtkTreeView *cview;
-    GtkListStore *cstore;
+    // An extra
+    GtkTreeView * cview;
+    GtkListStore * cstore;
 };
 
 /* A server information structure. It needs for appending a data on a list.
  * This data will used for a connection creating, when a user wants to
  * connect to a server from a server list. */
 struct con_info {
-    gchar *host;
-    gchar *uname;
-    gchar *passw;
-    gchar *name;
+    gchar * host;
+    gchar * uname;
+    gchar * passw;
+    gchar * name;
 };
 
 struct sel_data {
-    GtkTreeSelection *sel;
-    MYSQL *con;
+    GtkTreeSelection * sel;
+    MYSQL * con;
 };
 
 struct args_data {
-    const gchar *host;
-    const gchar *uname;
-    const gchar *passw;
+    const gchar * host;
+    const gchar * uname;
+    const gchar * passw;
 };
 
-#endif /* DATA_H */
+#define DATA_H
+#endif
